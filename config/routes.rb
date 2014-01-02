86f8b60cade9454274ee9f2704c085d884a26ab8
inkgab <keithar123@gmail.com>
2014-01-02 13:40:16 +0530

    Added controller plus css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rksbhadohi::Application.routes.draw do
+  resources :about
+
+  resources :collections
+
+  resources :contact
+
+  resources :events
+
+  get "welcome/index"
+ root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
